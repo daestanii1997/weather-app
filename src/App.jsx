71e21ab9ee35c5cf9fbf53e47a7c06616300d2ac
Dae'Stanii Spackman <daestanii1997@gmail.com>
2024-01-31 00:51:52 -0700

    passes props to forecast card and got weather to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,94 @@
-import ForecastCard from "./components/ForecastCard";
 import "./App.css";
 import { useState } from "react";
 import axios from "axios";
+import dayjs from "dayjs";
+import ForecastCard from "./components/ForecastCard";
 
 export default function App() {
-  const [cityInput, setCityInput] = useState('');
-  const [tempData, setTempData] = useState('')
+  const [cityInput, setCityInput] = useState("");
+  // Current
+  const [weatherData, setWeatherData] = useState("");
+  // Five Day
+  const [dayOneWeather, setDayOneWeather] = useState("");
+  const [dayOneDate, setDayOneDate] = useState("");
+  const [dayOneDesc, setDayOneDesc] = useState("");
+
+  const [dayTwoWeather, setDayTwoWeather] = useState("");
+  const [dayTwoDate, setDayTwoDate] = useState("");
+  const [dayTwoDesc, setDayTwoDesc] = useState("");
+
+  const [dayThreeWeather, setDayThreeWeather] = useState("");
+  const [dayThreeDate, setDayThreeDate] = useState("");
+  const [dayThreeDesc, setDayThreeDesc] = useState("");
+
+  const [dayFourWeather, setDayFourWeather] = useState("");
+  const [dayFourDate, setDayFourDate] = useState("");
+  const [dayFourDesc, setDayFourDesc] = useState("");
+
+  const [dayFiveWeather, setDayFiveWeather] = useState("");
+  const [dayFiveDate, setDayFiveDate] = useState("");
+  const [dayFiveDesc, setDayFiveDesc] = useState("");
 
   const apiKey = import.meta.env.VITE_API_KEY;
 
+  // Get Latitude and Longitude from City Input
   const geoCodeUrl =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
     cityInput +
     "&limit=5&appid=" +
     apiKey;
 
-  // Get Latitude and Longitude from City Input
   const getGeoCode = async () => {
-    const response = await axios.get(geoCodeUrl);
+    const geoResponse = await axios.get(geoCodeUrl);
 
+    // Get city Weather from Latitude and Longitude
     const weatherUrl =
       "https://api.openweathermap.org/data/2.5/weather?lat=" +
-      response.data[0].lat +
+      geoResponse.data[0].lat +
       "&lon=" +
-      response.data[0].lon +
+      geoResponse.data[0].lon +
       "&units=imperial&limit=5&appid=" +
       apiKey;
 
-    // Get city Weather from Latitude and Longitude
+    const fiveDayUrl =
+      "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      geoResponse.data[0].lat +
+      "&lon=" +
+      geoResponse.data[0].lon +
+      "&units=imperial&cnt=30&appid=" +
+      apiKey;
+
     const getWeather = async () => {
       const response = await axios.get(weatherUrl);
-      console.log(response.data);
-      setTempData(response.data.main.temp)
-      setCityInput('')
+      console.log("Response Data", response.data);
+      setWeatherData(response.data);
+
+      const forecastResponse = await axios.get(fiveDayUrl);
+      console.log("Forecast Data", forecastResponse.data);
+
+      setDayOneWeather(forecastResponse.data.list[0].main);
+      setDayOneDate(dayjs(forecastResponse.data.list[0].dt_txt).format('dddd, MMM D'));
+      setDayOneDesc(forecastResponse.data.list[0].weather[0].main)
+
+      setDayTwoWeather(forecastResponse.data.list[6].main);
+      setDayTwoDate(dayjs(forecastResponse.data.list[6].dt_txt).format('dddd, MMM D'));
+      setDayTwoDesc(forecastResponse.data.list[6].weather[0].main)
+
+      setDayThreeWeather(forecastResponse.data.list[14].main);
+      setDayThreeDate(dayjs(forecastResponse.data.list[14].dt_txt).format('dddd, MMM D'));
+      setDayThreeDesc(forecastResponse.data.list[14].weather[0].main)
+
+      setDayFourWeather(forecastResponse.data.list[22].main);
+      setDayFourDate(dayjs(forecastResponse.data.list[22].dt_txt).format('dddd, MMM D'));
+      setDayFourDesc(forecastResponse.data.list[22].weather[0].main)
+
+      setDayFiveWeather(forecastResponse.data.list[29].main);
+      setDayFiveDate(dayjs(forecastResponse.data.list[29].dt_txt).format('dddd, MMM D'));
+      setDayFiveDesc(forecastResponse.data.list[29].weather[0].main)
+
+      console.log(dayTwoDate)
+
+      setCityInput("");
     };
 
     getWeather();
@@ -50,10 +106,33 @@ export default function App() {
         Search
       </button>
 
-      {tempData ? 
-      <ForecastCard /> :
-      <p>Search your city!</p>}
+      {weatherData && (
+        <div>
+          <ForecastCard
+            dailyWeather={weatherData}
 
+            dayOneWeather={dayOneWeather}
+            dayOneDate={dayOneDate}
+            dayOneDesc={dayOneDesc}
+
+            dayTwoWeather={dayTwoWeather}
+            dayTwoDate={dayTwoDate}
+            dayTwoDesc={dayTwoDesc}
+
+            dayThreeWeather={dayThreeWeather}
+            dayThreeDate={dayThreeDate}
+            dayThreeDesc={dayThreeDesc}
+
+            dayFourWeather={dayFourWeather}
+            dayFourDate={dayFourDate}
+            dayFourDesc={dayFourDesc}
+
+            dayFiveWeather={dayFiveWeather}
+            dayFiveDate={dayFiveDate}
+            dayFiveDesc={dayFiveDesc}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -1,8 +1,102 @@
-export default function ForecastCard() {
-    return (
+import PropTypes from "prop-types";
 
+export default function ForecastCard(props) {
+  const {
+    dailyWeather,
+
+    dayOneWeather,
+    dayOneDate,
+    dayOneDesc,
+
+    dayTwoWeather,
+    dayTwoDate,
+    dayTwoDesc,
+
+    dayThreeWeather,
+    dayThreeDate,
+    dayThreeDesc,
+    
+    dayFourWeather,
+    dayFourDate,
+    dayFourDesc,
+    
+    dayFiveWeather,
+    dayFiveDate,
+    dayFiveDesc,
+} = props;
+
+  const weatherIcon = "https://openweathermap.org/img/wn/" + dailyWeather.weather[0].icon + "@2x.png"
+
+  return (
+    <div>
+      <section>
+        <h2>{dailyWeather.name}</h2>
+        <p>{dailyWeather.weather[0].main}</p>
+        <img src={weatherIcon}/>
+        <p>{dailyWeather.main.temp}° F</p>
+        <p>Feels Like: {dailyWeather.main.feels_like}° F</p>
+        <p>Min: {dailyWeather.main.temp_min}° F</p>
+        <p>Max: {dailyWeather.main.temp_max}° F</p>
+        <p>Humidity: {dailyWeather.main.humidity}%</p>
+      </section>
+
+      <section>
+        <h2>Five Day Forecast</h2>
         <div>
-        <p>The forecast card</p>
+          <h4>{dayOneDate}</h4>
+          <p>{dayOneDesc}</p>
+          <p>{dayOneWeather.temp}° F</p>
+          <p>Humidity: {dayOneWeather.humidity}%</p>
+        </div>
+        <div>
+          <h4>{dayTwoDate}</h4>
+          <p>{dayTwoDesc}</p>
+          <p>{dayTwoWeather.temp}° F</p>
+          <p>Humidity: {dayTwoWeather.humidity}%</p>
+        </div>
+        <div>
+          <h4>{dayThreeDate}</h4>
+          <p>{dayThreeDesc}</p>
+          <p>{dayThreeWeather.temp}° F</p>
+          <p>Humidity: {dayThreeWeather.humidity}%</p>
+        </div>
+        <div>
+          <h4>{dayFourDate}</h4>
+          <p>{dayFourDesc}</p>
+          <p>{dayFourWeather.temp}° F</p>
+          <p>Humidity: {dayFourWeather.humidity}%</p>
+        </div>
+        <div>
+          <h4>{dayFiveDate}</h4>
+          <p>{dayFiveDesc}</p>
+          <p>{dayFiveWeather.temp}° F</p>
+          <p>Humidity: {dayFiveWeather.humidity}%</p>
+        </div>
+      </section>
     </div>
-        )
+  );
 }
+
+ForecastCard.propTypes = {
+  dailyWeather: PropTypes.object,
+
+  dayOneWeather: PropTypes.any,
+  dayOneDate: PropTypes.string,
+  dayOneDesc: PropTypes.string,
+
+  dayTwoWeather: PropTypes.any,
+  dayTwoDate: PropTypes.string,
+  dayTwoDesc: PropTypes.string,
+
+  dayThreeWeather: PropTypes.any,
+  dayThreeDate: PropTypes.string,
+  dayThreeDesc: PropTypes.string,
+
+  dayFourWeather: PropTypes.any,
+  dayFourDate: PropTypes.string,
+  dayFourDesc: PropTypes.string,
+
+  dayFiveWeather: PropTypes.any,
+  dayFiveDate: PropTypes.string,
+  dayFiveDesc: PropTypes.string,
+};
